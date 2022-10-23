afb35e67ddbef90ecc859d1aa267bad2e5f45e3f
Alenna Castaneda <alenna.castaneda@oneidaihla.org>
2022-10-22 21:34:00 -0600

    redo 2

--- Objects.java
+++ Objects.java
@@ -10,102 +10,73 @@ class Pet {
     String owner;
     int age;
 
-    //Printing the pet details and their owner
-    void printPetDetails (Pet Pepper,Pet Snuggles,Pet Halia,Pet Daisy) {
-        Pet petName = new Pet();
-        if (Pepper != null) {
-            petName = Pepper;
-            System.out.println(petName.name+" is a "+petName.age+" year old "+petName.gender+" "+petName.species+" that belongs to "+petName.owner+".");
-        }
-        if (Snuggles != null) {
-            petName = Snuggles;
-            System.out.println(petName.name+" is a "+petName.age+" year old "+petName.gender+" "+petName.species+" that belongs to "+petName.owner+".");
-        }
-        if (Halia != null) {
-            petName = Halia;
-            System.out.println(petName.name+" is a "+petName.age+" year old "+petName.gender+" "+petName.species+" that belongs to "+petName.owner+".");
-        }
-        if (Daisy != null) {
-            petName = Daisy;
-            System.out.println(petName.name+" is a "+petName.age+" year old "+petName.gender+" "+petName.species+" that belongs to "+petName.owner+".");
-        }
-        }
+    //constructor
+    public Pet () {
+        name = "empty";
+        gender = "empty";
+        species = "empty";
+        owner = "empty";
+        age = 0;
+    }
 
+    //setting pet values
+    void setValues (String petName,String petGender,String petSpecies,String petOwner,int petAge) {
+        name = petName;
+        gender = petGender;
+        species = petSpecies;
+        owner = petOwner;
+        age = petAge;
+    }
+    //Printing the pet details and their owner
+    void printPetDetails () {
+        System.out.println(name+" is a "+age+" year old "+gender+" "+species+" that belongs to "+owner+".");
+    }
     //Changing owner to Nia
-    void claimPetOwnership (Pet Pepper,Pet Snuggles,Pet Halia,Pet Daisy) {
-        Pet petName = new Pet();
-        if (Pepper != null) {
-            petName = Pepper;
-            petName.owner = "Nia";
-            System.out.println(petName.name+" has been claimed by "+petName.owner+".");
-        }
-        if (Snuggles != null) {
-            petName = Snuggles;
-            petName.owner = "Nia";
-            System.out.println(petName.name+" has been claimed by "+petName.owner+".");
-        }
-        if (Halia != null) {
-            petName = Halia;
-            petName.owner = "Nia";
-            System.out.println(petName.name+" has been claimed by "+petName.owner+".");
-        }
-        if (Daisy != null) {
-            petName = Daisy;
-            petName.owner = "Nia";
-            System.out.println(petName.name+" has been claimed by "+petName.owner+".");
-        }
+    void claimPetOwnership (String newOwner) {
+        owner = newOwner;
+        System.out.println(name+" has been claimed by "+owner+".");
     }
     
     public static void main(String[] args) {
         //creating object named "Pepper"
         Pet Pepper = new Pet();
         //Giving value to Pepper's attributes
-        Pepper.name = "Pepper";
-        Pepper.gender = "female";
-        Pepper.species = "kitty cat";
-        Pepper.owner = "Melody";
-        Pepper.age = 12;
+        Pepper.setValues ("Pepper","female","kitty cat", "Melody", 12);
 
         //creating object named "Snuggles"
         Pet Snuggles = new Pet();
         //Giving value to Snuggles' attributes
-        Snuggles.name = "Snuggles";
-        Snuggles.gender = "male";
-        Snuggles.species = "catdog";
-        Snuggles.owner = "Melody";
-        Snuggles.age = 11;
+        Snuggles.setValues("Snuggles", "male", "catdog", "Melody", 11);
 
         //creating object named "Halia"
         Pet Halia = new Pet();
         //Giving value to Halia's attributes
-        Halia.name = "Halia";
-        Halia.gender = "female";
-        Halia.species = "pupcat";
-        Halia.owner = "Melody";
-        Halia.age = 2;
+        Halia.setValues("Halia", "female", "pupcat", "Melody", 2);
 
         //creating object named "Daisy"
         Pet Daisy = new Pet();
         //Giving value to Daisy's attributes
-        Daisy.name = "Daisy";
-        Daisy.gender = "female";
-        Daisy.species = "puppers";
-        Daisy.owner = "Nia";
-        Daisy.age = 2;
+        Daisy.setValues("Daisy", "female", "puppers", "Nia", 2);
 
-        //creating an object named "ob"
-        Pet ob = new Pet();
+        //creating a string of all pets
+        Pet[] allPets = {Pepper,Snuggles,Halia,Daisy};
 
         System.out.println("Melody and Nia have 4 pets. Nia LOVES them.");
-        //accessing ob method printPetDetails
-        ob.printPetDetails(Pepper, Snuggles, Halia, Daisy);
 
+        //printing pet details
+        for (int i=0; i < allPets.length; i++) {
+            allPets[i].printPetDetails();
+        }
         //adding a space
         System.out.println();
         System.out.println("Later.. Nia wants a fluffball army!");
 
-        //Nia claiming all of the pets
-        ob.claimPetOwnership(Pepper, Snuggles, Halia, null);
+        //Nia claiming all of the pets (that she doesn't already own)
+        for (int i=0; i < allPets.length; i++) {
+            if (allPets[i].owner != "Nia"){
+                allPets[i].claimPetOwnership("Nia");
+            }
+        }
 
          //adding a space
          System.out.println();
